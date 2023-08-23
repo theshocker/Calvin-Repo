de855bd843001c80801f71168e0d219a63423c5f
theshocker <93627312+theshocker@users.noreply.github.com>
2023-08-22 21:12:23 -0700

    Delete create_repo.tf

--- create_repo.tf
+++
@@ -1,9 +0,0 @@
-resource "github_repository" "test-repo" {
-  name        = "test-repo"
-  description = "Test repo from Terraform"
-  visibility = "public"
-  template {
-    owner      = "CalvinZhang"
-    repository = "python-fastapi-template"
-  }
-}
